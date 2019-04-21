c636374c86b12abc9bac7e57f78a05e5fdbd5ae4
isidro lucena santiago <darksirius91@gmail.com>
2019-04-21 23:13:36 +0200

    ji

--- redaccion 3/funciones.js
+++ redaccion 3/funciones.js
@@ -24,7 +24,28 @@ function checktel(){
 }
 function checkUsu(){
 var fltr=/'\d{8}/D/.'/;
-	var pue=document.getElementById("tel").value;
+	var pue=document.getElementById("usu").value;
 	var res=fltr.test(pue);
 	return res;
+}
+function trac(e){
+	var i=e.target
+	var id=i.id;
+	var res;
+	switch(id){
+		case "nm":res=checkname();break;
+		case "ape":res=checknApe();break;
+		case "dni":res=checknDni();break;
+		case "tel":res=checkTel();break;
+		case "usu":res=checkUsu();break;
+	}
+	if(res){
+		i.style.backgroundColor="#0CF908";
+	}else{
+		i.style.backgroundColor="#F91708";
+	}
+}
+var lis=document.getElementsByTagName("input");
+for(x=0;x<lis.length;x++){
+	lis[x].blur=trac(event);
 }
